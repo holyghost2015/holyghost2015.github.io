418dd0539ee9d5353f470017fed39baa41d2ef70
holyghost2015 <145147111+holyghost2015@users.noreply.github.com>
2023-09-22 20:27:46 +0200

    Update gameAZ.js

--- gameAZ.js
+++ gameAZ.js
@@ -56,6 +56,7 @@ var initialize = function(){
 
 var calculate = function(){
     for(var i=0; i<26; i++){
+        Amount[i]=+Amount[i];
         if(AmountProducers[i]>0){
             if(i==0){
                 Amount[0]+=AmountToProd[0]*AmountProducers[0];
